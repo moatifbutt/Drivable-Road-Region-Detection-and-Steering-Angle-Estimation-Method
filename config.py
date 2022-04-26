3934c37d0514bee1eeaf1c67d3b65507052e509e
aatiibutt <71174927+aatiibutt@users.noreply.github.com>
2022-04-26 13:47:50 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-EPOCHS = 50 # Number of the epochs
+EPOCHS = 100 # Number of the epochs
 SAVE_EVERY = 5 # after how many epochs to save a checkpoint
 LOG_EVERY = 1 #  log training and validation metrics every `LOG_EVERY` epochs
-BATCH_SIZE = 2 
+BATCH_SIZE = 4 
 DEVICE = 'cuda'  
 LR = 0.0001
 ROOT_PATH = 'ADD HERE FULL PATH OF THE DATASET'
